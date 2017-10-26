fcdb6e00d6e023e429c9f38430916f00cae596fa
Jordi Andreu <jandreu@cells.es>
2017-10-26 17:41:38 +0200

    [Fix] Expose Status class
    Expose Status class from constants to the main module.

--- smaract/__init__.py
+++ smaract/__init__.py
@@ -1,3 +1,3 @@
 from .controller import SmaractSDCController, SmaractMCSController
 from .communication import CommType
-from .constants import Direction, SensorMode, EffectorType
+from .constants import Direction, SensorMode, EffectorType, Status
