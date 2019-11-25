1e57c58840316c9b13afbb30a8c47359b035534b
Roberto Javier Homs Puron <rhoms@cells.es>
2019-11-25 10:44:46 +0100

    Bump version 0.4.0 to 0.5.0

--- smaract/__init__.py
+++ smaract/__init__.py
@@ -18,5 +18,5 @@ from .constants import Direction, SensorMode, EffectorType, Status
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
